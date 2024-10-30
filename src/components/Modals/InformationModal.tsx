66bfe91b02bd318df19ed07bfd1de4c65e6559f9
phroureo <castle91@gmail.com>
2024-10-30 09:44:35 -0500

    add more contact methods

--- src/components/Modals/InformationModal.tsx
+++ src/components/Modals/InformationModal.tsx
@@ -10,7 +10,7 @@ const InformationModal: React.FC<InformationModalProps> = ({ onClose }) => {
             <div className="save-modal-content">
                 <h2>This site was created by phroureo</h2>
                 <p>You can view the repo <a href="https://github.com/phroureo/pf2e-tools">here</a>, and submit issues if you find them.</p>
-                <p>You can also reach out to phroureo directly at <a href="mailto:me@phroureo.com">me@phroureo.com</a></p>
+                <p>You can also reach out to phroureo directly at <a href="mailto:me@phroureo.com">me@phroureo.com</a>, on Discord at phroureo, or on reddit at /u/phroureo</p>
                 <button className="confirm-button" onClick={onClose}>
                     Close
                 </button>
