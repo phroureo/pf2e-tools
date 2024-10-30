c7a4373b18bfa20b0f932849b8143fb00eba152d
phroureo <castle91@gmail.com>
2024-10-30 09:46:41 -0500

    add link to reddit message

--- src/components/Modals/InformationModal.tsx
+++ src/components/Modals/InformationModal.tsx
@@ -10,7 +10,7 @@ const InformationModal: React.FC<InformationModalProps> = ({ onClose }) => {
             <div className="save-modal-content">
                 <h2>This site was created by phroureo</h2>
                 <p>You can view the repo <a href="https://github.com/phroureo/pf2e-tools">here</a>, and submit issues if you find them.</p>
-                <p>You can also reach out to phroureo directly at <a href="mailto:me@phroureo.com">me@phroureo.com</a>, on Discord at phroureo, or on reddit at /u/phroureo</p>
+                <p>You can also reach out to phroureo directly at <a href="mailto:me@phroureo.com">me@phroureo.com</a>, on Discord at phroureo, or on reddit at <a href="https://www.reddit.com/message/compose/?to=phroureo">/u/phroureo</a></p>
                 <button className="confirm-button" onClick={onClose}>
                     Close
                 </button>
