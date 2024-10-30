33a709eae55fe6b5f44ef6b3deec8da89fbb8317
phroureo <castle91@gmail.com>
2024-10-30 10:17:40 -0500

    add link to discord server

--- src/components/Modals/InformationModal.tsx
+++ src/components/Modals/InformationModal.tsx
@@ -10,7 +10,7 @@ const InformationModal: React.FC<InformationModalProps> = ({ onClose }) => {
             <div className="save-modal-content">
                 <h2>This site was created by phroureo</h2>
                 <p>You can view the repo <a href="https://github.com/phroureo/pf2e-tools">here</a>, and submit issues if you find them.</p>
-                <p>You can also reach out to phroureo directly at <a href="mailto:me@phroureo.com">me@phroureo.com</a>, on Discord at <a href="https://discordapp.com/channels/@me/102549111017861120/">phroureo</a>, or on reddit at <a href="https://www.reddit.com/message/compose/?to=phroureo&subject=PF2eTools%20Inquiry">/u/phroureo</a>.</p>
+                <p>You can also reach out to phroureo directly at <a href="mailto:me@phroureo.com">me@phroureo.com</a>, on Discord at the <a href="https://discord.gg/wpzX2eK4sN">phroureo's PF2e Tools</a> server, or on reddit at <a href="https://www.reddit.com/message/compose/?to=phroureo&subject=PF2eTools%20Inquiry">/u/phroureo</a>.</p>
                 <button className="confirm-button" onClick={onClose}>
                     Close
                 </button>
